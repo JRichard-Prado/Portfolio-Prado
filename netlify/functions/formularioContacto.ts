41faa5a13c0dd0c0cf8234d3a1dfef52858e11be
JRichard-Prado <jrichip@hotmail.com>
2025-07-07 11:43:19 +0200

    handler export

--- netlify/functions/formularioContacto.ts
+++ netlify/functions/formularioContacto.ts
@@ -1,30 +1,30 @@
-import type { APIRoute } from "astro";
 import { Resend } from "resend";
 // import SampleEmail from "../../emails/sampleEmail";
 // import { render } from "@react-email/render";
 
 const resend = new Resend(process.env.RESEND_APIKEY);
 
-export const GET: APIRoute = async () => {
-  // create the email
+export const handler = async (event: any, context: any) => {
   // const emailContent = SampleEmail({ username: "Chris" });
   // const html = await render(emailContent);
-  // const text = await render(emailContent, {
-  //   plainText: true,
-  // });
+  // const text = await render(emailContent, { plainText: true });
 
-  // send an email
   const { data, error } = await resend.emails.send({
     from: "Richard <onboarding@resend.dev>",
     to: ["delivered@resend.dev"],
     subject: "It works!",
     html: `<p>Correo desde  contacto bootstrap modo static!</p>`,
-    
   });
 
   if (error) {
-    return new Response(JSON.stringify(error));
+    return {
+      statusCode: 500,
+      body: JSON.stringify(error),
+    };
   }
 
-  return new Response(JSON.stringify({data}));
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ data }),
+  };
 };

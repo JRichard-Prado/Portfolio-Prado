da94124cdf0793f791f777184faef28334937cb5
JRichard-Prado <jrichip@hotmail.com>
2025-07-07 09:54:39 +0200

    adapter  netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,13 @@ import { defineConfig } from 'astro/config';
 import netlify from '@astrojs/netlify';
 
 
-import vercel from '@astrojs/vercel';
+// import vercel from '@astrojs/vercel';
+// adapter: vercel()
 
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: vercel()
+  adapter: netlify(),
+  site: 'https://portfolio-prado.netlify.app',
 });

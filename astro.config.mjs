2105310ac9e3dd34d8ca0a12af851d83e36f1c12
JRichard-Prado <jrichip@hotmail.com>
2025-07-07 10:15:18 +0200

    modo static in netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,8 +9,8 @@ import netlify from '@astrojs/netlify';
 
 
 // https://astro.build/config
+// el adaptador de Netlify modo "server" no es compatible con Netlify, ya que Netlify solo soporta despliegues estáticos o funciones serverless, no servidores persistentes.
 export default defineConfig({
-  output: 'server',
+  output: 'static',
   adapter: netlify(),
-  site: 'https://portfolio-prado.netlify.app',
 });

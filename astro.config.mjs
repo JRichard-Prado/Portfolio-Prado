c17546856f14d7b12f332e5e9b16b5db0d808ecd
JRichard-Prado <jrichip@hotmail.com>
2025-07-09 11:29:25 +0200

    adapter netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config';
 
 
-// import netlify from '@astrojs/netlify';
+import netlify from '@astrojs/netlify';
 
 
-import vercel from '@astrojs/vercel';
+// import vercel from '@astrojs/vercel';
 // adapter: vercel()
 
 
@@ -12,5 +12,5 @@ import vercel from '@astrojs/vercel';
 // el adaptador de Netlify modo "server" no es compatible con Netlify, ya que Netlify solo soporta despliegues estáticos o funciones serverless, no servidores persistentes.
 export default defineConfig({
   output: 'server',
-  adapter: vercel(),
+  adapter: netlify(),
 });
